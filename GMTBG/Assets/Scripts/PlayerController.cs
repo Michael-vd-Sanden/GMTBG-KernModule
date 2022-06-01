85174e68617549ce7265194c3120bbf97b273518
Michael_vd_Sanden <michael.mvdsanden@gmail.com>
2022-06-01 18:43:47 +0200

    Added Sprites

---
+++ GMTBG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int maxHealth = 5;
+    private int currentHealth;
+
+    public float timeInvincible = 2f;
+    private bool isInvincible;
+    private float invincibleTimer;
+
+    //[SerializeField] private Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    public int health
+    {
+        get
+        { return currentHealth; }
+    }
+
+    new Rigidbody2D rigidbody;
+    float horizontal;
+    float vertical;
+
+    public float speed = 3f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+
+        //animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+       // animator.SetFloat("Look X", lookDirection.x);
+       // animator.SetFloat("Look Y", lookDirection.y);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            { isInvincible = false; }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+
+        rigidbody.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            { return; }
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
---
+++ GMTBG/Assets/Scripts/SpriteRenderOrder.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRenderOrder : MonoBehaviour
+{
+    void Start()
+    {
+        SpriteRenderer[] renderers = FindObjectsOfType<SpriteRenderer>();
+
+        foreach (SpriteRenderer renderer in renderers)
+        {
+            renderer.sortingOrder = (int)(renderer.transform.position.y * -100);
+        }
+    }
+
+}
